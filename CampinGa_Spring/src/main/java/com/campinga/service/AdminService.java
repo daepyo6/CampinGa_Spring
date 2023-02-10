5083b72a7e63d55599f824508643f7a55b267726
JAVA01 <JAVA01@DESKTOP-T35QNKU>
2023-02-10 12:02:44 +0900

    로그인

--- CampinGa_Spring/src/main/java/com/campinga/service/AdminService.java
+++ CampinGa_Spring/src/main/java/com/campinga/service/AdminService.java
@@ -1,14 +1,27 @@
 package com.campinga.service;
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.campinga.dao.IAdminDao;
+import com.campinga.dto.Paging;
+
 
 @Service
 public class AdminService {
 	
 	@Autowired
 	IAdminDao adao;
+
+	public void getAdminList(HashMap<String, Object> paramMap) {
+		adao.getAdminList(paramMap);
+	}
+
+
 
 }
